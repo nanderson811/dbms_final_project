01f55b6a17701fd6245130286562986e5205f343
Noah Anderson <andersonn11@uww.edu>
2023-04-28 17:29:11 -0500

    changed test cases

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 db = Database(username="andersonn11", password="na5146", host="140.146.23.39", port=3306, db="cs366-2231_andersonn11")
 mymap = MapInterface(41.8781, -87.6298, 13)
-results = db.callProcedure('distanceWithin', [5280*100, 1169000, 1852000])
+results = db.callProcedure('distanceWithin', [5280, 1169000, 1852000])
 start = time.time()
 crime_coords = []
 for row in results:
@@ -25,6 +25,8 @@ for coord in crime_coords:
     mymap.place_marker(lat, lon)
 
 end = time.time()
+
+
 
 print("time 3: " + str(end-start))
 mymap.draw_map("chicago_map.html")

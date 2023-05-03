0a44b74250d188a2d884a311de906871834c87e2
Noah Anderson <andersonn11@uww.edu>
2023-05-03 18:51:39 -0500

    added pin click functionality, more user options for customizing selection

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, redirect, url_for
 from database import Database
 from mapinterface import MapInterface
 from userinterface import UserInterface
-import time
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -11,39 +10,18 @@ app.add_url_rule('/templates/chicago_map.html', 'chicago_map', lambda: render_te
 db = Database(username="andersonn11", password="na5146", host="140.146.23.39", port=3306, db="cs366-2231_andersonn11")
 mymap = MapInterface(41.8781, -87.6298, 13)
 ui = UserInterface()
-mymap.draw_map('templates/chicago_map.html')
-
+# removes excess chicago_map files that may still exist upon running the program
+# for the first time
+for i in range(1000):
+    if os.path.exists('templates/'+str(i)+'chicago_map.html'):
+        os.remove('templates/'+str(i)+'chicago_map.html')
 
 
 @app.route('/')
 def index():
     if os.path.exists('templates/'+str(mymap.get_counter())+'chicago_map.html'):
         os.remove('templates/'+str(mymap.get_counter())+'chicago_map.html')
-    return render_template('index.html')
-
-
-@app.route('/distance', methods=['POST'])
-def distance(x, y):
-    results = db.callProcedure('distanceWithin', [5280, x, y])
-    start = time.time()
-    crime_coords = []
-    for row in results:
-        if row[1] == 'HOMICIDE':
-            crime_coords.append([row[4], row[5]])
-    end = time.time()
-
-    print("time 1: " + str(end-start))
-
-    start = time.time()
-    for coord in crime_coords:
-        lat, lon = ui.convert(coord[0], coord[1])
-        mymap.place_marker(lat, lon)
-
-    end = time.time()
-
-    print("time 3: " + str(end-start))
-
-    return "Map generated for distance: "
+    return render_template('index.html', color=mymap.get_color())
 
 
 @app.route('/get_location', methods=['POST'])
@@ -56,14 +34,8 @@ def get_location():
     x, y = ui.convertToXY(lat, lng)
     mymap.place_user(lat, lng, radius*0.3060)
     results = db.callProcedure('distanceWithin', [radius, x, y])
-    crime_coords = []
-    for row in results:
-        if row[1] == crime_type:
-            crime_coords.append([row[4], row[5]])
-    for coord in crime_coords:
-        lat, lon = ui.convert(coord[0], coord[1])
-        mymap.place_marker(lat, lon)
 
+    draw_points(crime_type, results)
     mymap.add_to_counter()
     filename = "templates/chicago_map.html"
     mymap.draw_map(filename)
@@ -90,6 +62,27 @@ def show_map():
 def clear_map():
     mymap.clear_map()
     return redirect('/')
+
+
+@app.route('/set_color', methods=['POST'])
+def set_color():
+    color = request.form['colorselect']
+    mymap.color(color)
+    return redirect('/')
+
+
+def draw_points(crime_type, data):
+
+    for row in data:
+
+        if row[1] == crime_type:
+            lat, lon = ui.convert(row[4], row[5])
+            mymap.place_marker(lat, lon, crime_type, row[0], distance_from_center=format(row[3], '.1f'))
+
+
+@app.route('/other_options', methods=['POST'])
+def other_options():
+    return render_template('other_options.html')
 
 
 if __name__ == '__main__':
--- mapinterface.py
+++ mapinterface.py
@@ -4,6 +4,7 @@ from gmplot import gmplot
 # class to manage the map interface
 class MapInterface:
     def __init__(self, start_lat, start_long, zoom_level):
+        self.__color = "red"
         self.__start_lat = start_lat
         self.__start_long = start_long
         self.__zoom_level = zoom_level
@@ -17,8 +18,27 @@ class MapInterface:
         self.__gmap.draw(filename)
 
     # places a blank marker down at the lat/long coordinates
-    def place_marker(self, lat, long):
-        self.__gmap.marker(lat, long)
+    def place_marker(self, lat, long, crimetype, crime_id, distance_from_center='', arrest_made='', is_domestic='',
+                     date_occurred='', date_updated='', case_number='', case_desc=''):
+        marker_desc = "Crime Type: " + crimetype + "<br>Latitude: " + format(lat, '.4f')
+        marker_desc += "<br>Longitude: " + format(long, '.4f')
+        marker_desc += "<br>Crime ID: " + str(crime_id)
+        if case_number != '':
+            marker_desc += "<br>Case Number: " + case_number
+        if case_desc != '':
+            marker_desc += "<br>Case Description: " + case_desc
+        if distance_from_center != '':
+            marker_desc += "<br>Distance from Center: " + distance_from_center + " ft"
+        if arrest_made != '':
+            marker_desc += "<br>Date Occurred: " + str(date_occurred)
+        if is_domestic != '':
+            marker_desc += "<br>Date Updated: " + str(date_updated)
+        if date_occurred != '':
+            marker_desc += "<br>Date Occurred: " + str(date_occurred)
+        if date_updated != '':
+            marker_desc += "<br>Date Updated: " + str(date_updated)
+
+        self.__gmap.marker(lat, long, self.__color, title=crimetype, info_window=marker_desc)
 
     # places a blue marker as the user, and a translucent
     # white circle around it with provided radius
@@ -35,3 +55,9 @@ class MapInterface:
     def clear_map(self):
         self.__gmap = gmplot.GoogleMapPlotter(self.__start_lat, self.__start_long, self.__zoom_level,
                                               apikey=self.__apikey)
+
+    def color(self, color):
+        self.__color = color
+
+    def get_color(self):
+        return self.__color
